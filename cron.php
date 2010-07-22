ff9ef044eb91ed883db21e1faf6bd1bf1a9e4848
Helmut Poppen <helmut@coffeecoder.de>
2010-07-22 22:49:52 +0800

    fixed the cron example

--- cron.php
+++ cron.php
@@ -16,14 +16,14 @@
 	$ssm = new Sosumi('your_username', 'your_password');
 	$loc = $ssm->locate();
 
-	if(strlen($loc->latitude))
+	if(strlen($loc['latitude']))
 	{
 		$db = mysql_connect('localhost', 'root', '');
 		mysql_select_db('sosumi', $db) or die(mysql_error());
 
 		$dt = date('Y-m-d H:i:s');
-		$lat = mysql_real_escape_string($loc->latitude, $db);
-		$lng = mysql_real_escape_string($loc->longitude, $db);
+		$lat = mysql_real_escape_string($loc['latitude'], $db);
+		$lng = mysql_real_escape_string($loc['longitude'], $db);
 
 		$query = "INSERT INTO history (`dt`, `lat`, `lng`) VALUES ('$dt', '$lat', '$lng')";
 		mysql_query($query, $db) or die(mysql_error());
